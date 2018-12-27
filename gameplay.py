77605d786dffcda39a14893672ffdadd3f864d98
Kimana Otto <ko@iquer.net>
2018-12-27 12:16:33 +0100

    added starting scene

---
+++ gameplay.py
@@ -0,0 +1,53 @@
+import textwrap
+
+
+class Adventure(object):
+    def prompt(self):
+        x = input("Type a command: ")
+        return(x)
+
+    def __init__(self):
+        self.gold = 2
+        self.health = 10
+        self.energy = 10
+        self.melee = 1
+        self.ranged = 0
+        self.ammo = 0
+        self.full_health = 10
+        self.attack_num = 0
+
+    def checkstats(self):
+        global = ranged
+        print('''
+        ********''')
+        print("Gold:", self.gold)
+        print("Health:", self.health, "/", self.full_health)
+        print("Melee:", self.melee)
+        print ("Energy:", self.energy)
+        
+        if ranged > 0:
+            print("Ranged:", ranged)
+            print("Ammo:", ammo)
+
+    def start(self):
+        msg = textwrap.dedent('''
+        ****************************************
+        *                                      *
+        *      *Welcome to Jimmy's World*
+        *                                      *
+        ****************************************
+        ''')
+        print(msg)
+        self.StartingRoom()
+          command = prompt()
+           if command == "1":
+                Room_description()
+            elif command == "2":
+                print("********")
+                print (
+                    "You're hiding behind a chair thats right in front of you. You hear a strange noise from the back of the room.")
+            elif command == "9":
+                checkstats()
+                StartingRoom()
+            else:
+                StartingRoom()
--- tiles.py
+++ tiles.py
@@ -1,4 +1,6 @@
 import items
+import textwrap
+
 
 
 class MapTile:
@@ -6,46 +8,69 @@ class MapTile:
         self.x = x
         self.y = y
 
-def intro_text(self):
-    raise NotImplementedError()
-
-def modify_player(self, player):
-    raise NotImplementedError()
-    
-class StartingRoom(MapTile):
     def intro_text(self):
-        return """
-        You find yourself in a big, square room.
-        It looks abandoned and smells musty.
-        """
- 
-    def modify_player(self, player):
-        #Room has no action on player
-        pass
-
-
-class LootRoom(MapTile):
-    def __init__(self, x, y, item):
-        self.item = item
-        super().__init__(x, y)
-
-    def add_loot(self, player):
-        player.inventory.append(self.item)
+        raise NotImplementedError()
 
     def modify_player(self, player):
-        self.add_loot(player)
-    
-    
-class EnemyRoom(MapTile):
-    def __init__(self, x, y, enemy):
-        self.enemy = enemy
-        super().__init__(x, y)
+        raise NotImplementedError()
 
-    def modify_player(self, the_player):
-        if self.enemy.is_alive():
-            the_player.hp = the_player.hp - self.enemy.damage
-            print("Enemy does {} damage. You have {} HP remaining.".format(
+
+########################################### Rooms #############################################   
+
+
+    class StartingRoom(MapTile):
+        def intro_text(self):
+            print("""
+            You find yourself in a big, square room.
+            It looks abandoned and smells musty.
+            """)
+            print("""Options: 
+            (1) look around 
+            (2) hide
+            (9) show stats""")
+
+
+        def Room_description():
+            print("""
+             
+            """)
+
+
+
+            
+        def modify_player(self, player):
+            #Room has no action on player
+            pass
+
+
+    class LootRoom(MapTile):
+        def __init__(self, x, y, item):
+            self.item = item
+            super().__init__(x, y)
+
+        def add_loot(self, player):
+            player.inventory.append(self.item)
+
+        def modify_player(self, player):
+            self.add_loot(player)
+                
+                
+    class EnemyRoom(MapTile):
+        def __init__(self, x, y, enemy):
+            self.enemy = enemy
+            super().__init__(x, y)
+
+        def modify_player(self, the_player):
+            if self.enemy.is_alive():
+                the_player.hp = the_player.hp - self.enemy.damage
+                print("Enemy does {} damage. You have {} HP remaining.".format(
                 self.enemy.damage, the_player.hp))
 
 
+        def main():
+            adventure = Adventure()
+            adventure.start()
 
+
+            if __name__ == '__main__':
+                main()
